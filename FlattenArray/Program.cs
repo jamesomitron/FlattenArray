4432fd4400a764f421258fff7da985c7803cd5c8
James Omitiran <james.omitiran@outlook.com>
2018-12-14 14:48:16 +0100

    Add project files.

---
+++ FlattenArray/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlattenArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
